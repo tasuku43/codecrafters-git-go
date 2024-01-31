f634abbf5af3f84a485ba624a1ca7b271066135c
tasuku43 <task.teckac@gmail.com>
2024-02-01 06:46:52 +0900

    pass 2st stage

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
@@ -26,7 +29,47 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+	case "cat-file":
+		if len(os.Args) != 4 || os.Args[2] != "-p" {
+			fmt.Fprintf(os.Stderr, "Error: usage: your_git.sh cat-file -p <hash>\n")
+			os.Exit(1)
+		}
 
+		hash := os.Args[3]
+		file, err := os.Open(fmt.Sprintf(".git/objects/%s/%s", hash[:2], hash[2:]))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+
+		zr, err := zlib.NewReader(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating zlib reader: %s\n", err)
+			os.Exit(1)
+		}
+		defer zr.Close()
+
+		var content strings.Builder
+		buf := make([]byte, 1024)
+		for {
+			n, err := zr.Read(buf)
+			if err != nil && err != io.EOF {
+				fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+				os.Exit(1)
+			}
+			if n == 0 {
+				break
+			}
+			content.Write(buf[:n])
+		}
+
+		splitContent := strings.SplitN(content.String(), "\x00", 2)
+		if len(splitContent) != 2 {
+			fmt.Fprintf(os.Stderr, "Error: Invalid object format\n")
+			os.Exit(1)
+		}
+		fmt.Print(splitContent[1])
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
